73b7058bb761d4aaa9ffa4cec628af9976ffbea8
Zian Ejaz <zianejaz@gmail.com>
2021-05-11 17:52:42 +0500

    added word count in header input text

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,15 @@ import '../style.css';
 
 class Header extends Component {
     state = {
-        keyword: 'hello'
+        keyword: 'hello',
+        wordcount : 0
     }
-inputChange = (event)=>{
-    this.setState({keyword:event.target.value})
+    inputChange = (event) => {
+        this.setState({ keyword: event.target.value })
+        this.setState({ wordcount: event.target.value.split(" ").length })
         
- 
-  }
+
+    }
 
 
     render() {
@@ -24,7 +26,8 @@ inputChange = (event)=>{
                     </ul>
                 </nav>
                 <input type="text" placeholder="Enter Something" onChange={this.inputChange}></input>
-                <h1>{this.state.keyword}</h1>
+                <h1>{this.state.wordcount}</h1>
+                <p>{this.state.keyword}</p>
 
             </header>
         )
