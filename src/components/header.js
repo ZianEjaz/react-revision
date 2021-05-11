e4547241eeb6f6dac57ce5a973c09c83c781b210
Zian Ejaz <zianejaz@gmail.com>
2021-05-11 16:36:16 +0500

    added Header styles and made Class based Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,34 @@
+import React, { Component } from 'react';
+import '../style.css';
 
-const Header = ()=>{
-    return(
-        <header >
-            <h1>HEADER</h1>
-            <nav>
-                <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/about'>About</a></li>
-                    <li><a href='/users'>Users</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+class Header extends Component {
+    state = {
+        keyword: 'hello'
+    }
+inputChange = (event)=>{
+    this.setState({keyword:event.target.value})
+        
+ 
+  }
+
+
+    render() {
+        return (
+            <header>
+                <h1>HEADER</h1>
+                <nav>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/about'>About</a></li>
+                        <li><a href='/users'>Users</a></li>
+                    </ul>
+                </nav>
+                <input type="text" placeholder="Enter Something" onChange={this.inputChange}></input>
+                <h1>{this.state.keyword}</h1>
+
+            </header>
+        )
+    }
 }
 
 export default Header
