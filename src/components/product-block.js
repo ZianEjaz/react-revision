c3c18a1b2061843ed8192c20e377fdd18ffe116c
Zian Ejaz <zianejaz@gmail.com>
2021-05-30 19:41:55 +0500

    .

--- src/components/product-block.js
+++ src/components/product-block.js
@@ -7,7 +7,7 @@ return(
                   className="m-auto p-7 max-w-xs" src={props.product.image} alt="placeholder"                ></img>
                 <p className="w-80 mx-auto text-lg">{props.product.name} {props.product.sku}</p>
                 <p className="w-80 center mx-auto text-sm">{props.product.description} </p>
-                <a href={props.product.url} target='blank' className="w-80 center mx-auto text-sm">Read More </a>
+                <a href={props.product.url} target='blank' className="bg-indigo-700 text-white p-4 rounded m-5 block">Read More </a>
                 
             
               </div>)
