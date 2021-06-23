bf4157be5c485a6418ddc6c92da81915ecb7ebd2
Zian Ejaz <zianejaz@gmail.com>
2021-06-24 03:05:54 +0500

    added tailwind css and some styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import '../style.css';
+import React from "react";
+import "../style.css";
 
- const Header = ()=>{
-        return (
-            <header>
-                <h1>HEADER</h1>
-                <nav>
-                    <ul>
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/about'>About</a></li>
-                        <li><a href='/users'>Users</a></li>
-                        <li><a href='/jsontoproduct'>Json data to product block</a></li>
-                        <li><a href='/removespaces'>remove spaces</a></li>
-                    </ul>
-                </nav>
-                
+const Header = () => {
+  return (
+    <header className="w-full">
+      <nav className="bg-gray-700 text-center md:flex">
+        <div className="w-full md:w-3/12">
+          <h1 className='text-2xl text-white'>logo</h1>
+        </div>
+        <div className="text-white p-2 w-full md:w-9/12 ">
+        <ul className=" inline-flex w-full justify-center md:justify-end">
+          <li className="p-2">
+            <a href="/">Home</a>
+          </li>
+          <li className="p-2 ">
+            <a href="/about">About</a>
+          </li>
+          <li className="p-2 ">
+            <a href="/users">Users</a>
+          </li>
+          <li className="p-2 ">
+            <a href="/jsontoproduct">Json to block</a>
+          </li>
+          <li className="p-2 ">
+            <a href="/removespaces">remove spaces</a>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-            </header>
-        )
-    }
-
-
-export default Header
+export default Header;
--- src/components/remove-spaces.js
+++ src/components/remove-spaces.js
@@ -1,38 +1,39 @@
 import React, { Component } from "react";
 
-
-class RemoveSpaces extends Component{
-    state={
-        filteredString : 'Enter data to remove spaces'
-    }
-    inputChange =  (event) => {
-        let string = event.target.value;
-      let  filteredString = string.replace(/\s/g, '')
-        this.setState(
-            {filteredString}
-        )
-
-    }
-    //copy code to clipboard
-    copyCodeToClipboard = () => {
-    const el = this.textArea
-    el.select()
-    document.execCommand("copy")
-    this.setState({filteredString : ''})
+class RemoveSpaces extends Component {
+  state = {
+    filteredString: "Enter data to remove spaces",
+  };
+  inputChange = (event) => {
+    let string = event.target.value;
+    let filteredString = string.replace(/\s/g, "");
+    this.setState({ filteredString });
+  };
+  //copy code to clipboard
+  copyCodeToClipboard = () => {
+    const el = this.textArea;
+    el.select();
+    document.execCommand("copy");
+    this.setState({ filteredString: "" });
+  };
+  render() {
+    return (
+      <div className='w-11/12 mx-auto'>
+          <div className='w-full mx-auto'>
+        <textarea className='w-full mx-auto shadow-md mt-4 p-3 bg-gray-100 text-gray-800'
+          onChange={this.inputChange}
+          ref={(textarea) => (this.textArea = textarea)}
+          value={this.state.filteredString}
+        />
+        </div>
+        <div className='w-full mx-auto'>
+        <button className='w-full mx-auto bg-gray-800 p-3 my-2 text-white shadow-md' onClick={() => this.copyCodeToClipboard()}>
+          Click To Copy to Clipboard Spaces Removed And Clear Text Box 
+        </button>
+        </div>
+      </div>
+    );
   }
-    render(){
-        return(
-            <div  style={{width: '80%',margin : ' 10px auto'}}>
-                <textarea onChange={this.inputChange}
-            ref={(textarea) => this.textArea = textarea}
-            value={this.state.filteredString}
-          />
-                <button onClick={() => this.copyCodeToClipboard()}>
-            Copy to Clipboard
-          </button>
-            </div>
-        )
-    }
 }
 
-export default RemoveSpaces
+export default RemoveSpaces;
